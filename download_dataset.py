df15dc1ee77776db0e74044c442f241edef6e583
nyankahmensah <nyankahmensah@gmail.com>
2020-06-09 09:38:12 +0000

    try me

--- download_dataset.py
+++ download_dataset.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from lib.config import DATASET_ROOT
 
 def download_dataset():
@@ -9,6 +10,7 @@ def download_dataset():
 	# check if the dataset file has already been downloaded
 	# otherwise download it
 	# (ano get data like that😂)
+	print("👾 downloading dataset file")
 	dataset_rar = os.path.join(DATASET_ROOT, "dataset.rar")
 	if not os.path.exists(dataset_rar):
 		dataset_url = "https://www.crcv.ucf.edu/data/UCF101/UCF101.rar"
@@ -28,9 +30,13 @@ def download_dataset():
 			videos_path
 		])
 		os.system(command)
+		print("👾 file downloaded and extracted")
+	else:
+		print("👾 file already exists, skipping")
 
 	# check if the annotations file has already been download
 	# otherwise download it
+	print("👾 downloading annotations file")
 	annotations_zip = os.path.join(DATASET_ROOT, "annotations.zip")
 	if not os.path.exists(annotations_zip):
 		annotations_url = "https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip"
@@ -50,6 +56,35 @@ def download_dataset():
 			"-d", annotations_path
 		])
 		os.system(command)
+		print("👾 file downloaded and extracted")
+	else:
+		print("👾 file already exists, skipping")
 
 def decode_videos_to_frames():
 	pass
+	# checking if frames root exist otherwise create it
+	#if not os.path.exists(DATASET_ROOT):
+	#	os.makedirs(DATASET_ROOT)
+
+def build_file_list():
+	pass
+
+if __name__ == '__main__':
+	# get arguments from terminal
+	parser = argparse.ArgumentParser(description = "prepare UCF101 dataset")
+    parser.add_argument('--download', action='store_true', default=True)
+    parser.add_argument('--decode_video', action='store_true', default=True)
+    parser.add_argument('--build_file_list', action='store_true', default=True)
+    args = parser.parse_args()
+
+    if args.download:
+        print('🤖 downloading UCF101 dataset.')
+        download_dataset()
+
+    if args.decode_video:
+        print('🤖 decoding videos to frames.')
+        decode_videos_to_frames()
+
+    if args.build_file_list:
+        print('🤖 generating training files.')
+        build_file_list()
--- lib/config.py
+++ lib/config.py
@@ -2,14 +2,6 @@ import os
 
 DATASET_ROOT = "/dataset"
 
-TRAIN_DATASET_ROOT = os.path.join(DATASET_ROOT, "train")
-TEST_DATASET_ROOT = os.path.join(DATASET_ROOT, "test")
-VALID_DATASET_ROOT = os.path.join(DATASET_ROOT, "valid")
-
-TRAIN_FRAMES_ROOT = os.path.join(DATASET_ROOT, "train_frames")
-TEST_FRAMES_ROOT = os.path.join(DATASET_ROOT, "test_frames")
-VALID_FRAMES_ROOT = os.path.join(DATASET_ROOT, "valid_frames")
-
 EPOCHS = 50
 IMAGE_SIZE = (256, 256)
 BATCH_SIZE = 32
