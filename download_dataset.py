9c81db24ec87b9d9f7efb66a64a63839b5a0f751
Enoch Nana Nyankah Mensah <nyankahmensah@gmail.com>
2020-06-10 19:26:36 +0000

    trying something new

--- download_dataset.py
+++ download_dataset.py
@@ -191,6 +191,13 @@ def build_list():
     df.to_csv(filename, index = False)
     print(" file saved")
 
+def crop_center_square(frame):
+  y, x = frame.shape[0:2]
+  min_dim = min(y, x)
+  start_x = (x // 2) - (min_dim // 2)
+  start_y = (y // 2) - (min_dim // 2)
+  return frame[start_y:start_y+min_dim,start_x:start_x+min_dim]
+  
 def build_video_list():
   splits = glob.glob("{}/*list*.txt".format(ANNOTATIONS_ROOT))
   print(splits)
