80d942e6484d03fff3a2a484e85c2a567db39815
Enoch Nana Nyankah Mensah <nyankahmensah@gmail.com>
2020-06-11 05:27:33 +0000

    trying something new

--- lib/config.py
+++ lib/config.py
@@ -5,9 +5,9 @@ FRAMES_ROOT = os.path.join(DATASET_ROOT, "frames")
 VIDEOS_ROOT = os.path.join(DATASET_ROOT, "videos")
 ANNOTATIONS_ROOT = os.path.join(DATASET_ROOT, "annotations")
 
-EPOCHS = 50
+EPOCHS = 10
 IMAGE_SIZE = (128, 128)
-BATCH_SIZE = 2
+BATCH_SIZE = 16
 WORKERS = 8
 
 MODEL_ROOT = "model"
