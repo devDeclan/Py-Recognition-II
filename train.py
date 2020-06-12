2f7756fb7efaa82acf2b26fe1ef8df7516c9d5f7
Enoch Nana Nyankah Mensah <nyankahmensah@gmail.com>
2020-06-12 15:38:24 +0000

    made some changes

--- train.py
+++ train.py
@@ -139,7 +139,7 @@ def main(list_number = 1):
 
   # save model
   "🤖 saving model"
-  keras.experimental.export_saved_model(model, path.join(MODEL_ROOT, "model.h5"))
+  tf.compat.v1.keras.experimental.export_saved_model(model, path.join(MODEL_ROOT, "model.h5"))
 
   # convert model to json
   "🤖 saving model to json"
